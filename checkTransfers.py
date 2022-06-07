cf63e83910eea0ea16e8c3b2199b25cb67dfddcd
Thomas Matlock <thomas.matlock@ergon.com>
2022-06-07 14:53:45 -0500

    Add notes

--- checkTransfers.py
+++ checkTransfers.py
@@ -3,11 +3,13 @@ import os
 import pandas as pd
 import numpy as np
 
+from checkFolder import AdListItems
+
+path="C:\\Users\\tmatlock\\Documents\\SystemAccessAudit"
 
 #Read all Folder Contents
 os.chdir('C:\\Users\\tmatlock\\Documents\\SystemAccessAudit')
 DIR = os.listdir()
-#print(DIR)
 
 #Partial File Name
 transferName = 'Transfers'
@@ -19,7 +21,7 @@ transferList = []
 adList = []
 
 
-#print(DIR)
+
 for f in DIR:
     #Get only Transfer Files
     if transferName == f[:len(transferName)]:
@@ -27,6 +29,32 @@ for f in DIR:
     if adName == f[:len(adName)]:
         adList.append(f)
 
-#print(transferList)
 
-counter = 1
+AdListItems = []
+TransferListItems = []  
+
+for item in transferList: 
+    TransferListItems.append(pd.read_excel(path+'\\'+item).to_records())
+
+for item in adList:
+    AdListItems.append(pd.read_csv(path+'\\'+item).to_records())
+
+transferedEmployee = []
+adEmployee = []
+
+for record in TransferListItems:
+    for x in record:
+        for j in x: 
+            if j == "Transfer":
+                transferedEmployee.append(x)
+
+empList = []
+
+for transferRecord in transferedEmployee:
+    for item in AdListItems:
+        for x in item:
+            if transferRecord[3] == x[2]:
+                if isinstance(x[2], float):
+                    empList.append(int(x[2]))
+
+print(empList)
