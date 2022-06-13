8ac61762d9c50e5f81b2a62c26e36512d7d8a71c
Thomas Matlock <thomas.matlock@ergon.com>
2022-06-13 09:51:42 -0500

    Add Dropzone

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const path = require('path')
 
-
 const express = require('express')
+const { Dropzone } = require('dropzone')
 
 //Create instance of express
 const app = express()
 const port = process.env.PORT || 3000 // Port 3000
 
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 // Use Static Files to serve images and css
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Home Route
 app.get('/', (req, res, next) => {
-    res.send("hello World")
+    let myDropzone = new Dropzone("#my-form");
+    myDropzone.on("addedfile", file => {
+    console.log(`File added: ${file.name}`);
+    });
 })
 
 // LISTEN
