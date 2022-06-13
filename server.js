6e80f7769e4f6439308294b5919bb6df3cae76c0
Thomas Matlock <thomas.matlock@ergon.com>
2022-06-13 09:12:24 -0500

    Add express.static

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+const path = require('path')
+
+
 const express = require('express')
 
+//Create instance of express
 const app = express()
+const port = process.env.PORT || 3000 // Port 3000
 
-const port = 3000
+// Use Static Files to serve images and css
+app.use(express.static(path.join(__dirname, 'public')));
 
+//Home Route
 app.get('/', (req, res, next) => {
     res.send("hello World")
 })
 
+// LISTEN
 app.listen(port, () => {
     console.log('Connected')
 })
